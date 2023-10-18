8f92dcf3e8590f7ec8c814ebcad1fba8a1591119
Abhishek Panda <abxisxekpanda@gmail.com>
2023-10-18 09:23:03 +0530

    AST Fixes

--- src/minifier.js
+++ src/minifier.js
@@ -4,8 +4,25 @@ import * as acorn from 'acorn';
 import escodegen from 'escodegen';
 
 export default class Minifier {
+  #nameMap = new Map();
+  #alphabet = Array.from('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+  #generateNameIfNotExisting(name) {
+    if (this.#nameMap.has(name)) {
+      return this.#nameMap.get(name);
+    }
+    if (!this.#alphabet) {
+      throw new Error('No more names vailable - limit is 52 tokens');
+    }
+    const newName = this.#alphabet.shift();
+    this.#nameMap.set(name, alphabet);
+    return newName;
+  }
   minifyCodeAndReturnMapNames(originalCode) {
     const originalAST = acorn.parse(originalCode, {ecmaVersion: 2022, locations: true});
-    console.log(JSON.stringify(originalAST, null, 2));
+    const minifiedCode = escodegen.generate(originalAST, {format: {compact: true}});
+    console.log('createUsers', this.#generateNameIfNotExisting('createUsers'));
+    console.log('createUsers', this.#generateNameIfNotExisting('createUsers'));
+    console.log('select', this.#generateNameIfNotExisting('createUsers'));
   }
 }
