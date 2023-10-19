282388469b847fb9d16c7380f2454e787ac2ded2
Abhishek Panda <abxisxekpanda@gmail.com>
2023-10-19 22:21:17 +0530

    Mappings fixed

--- src/minifier.js
+++ src/minifier.js
@@ -1,28 +1,71 @@
-// generate AST (Abstract Syntax Tree)
+// generate ast
 import * as acorn from 'acorn';
 // validates js code and remove comments and line breaks
 import escodegen from 'escodegen';
+import ASTHelper from './ast-helper.js';
 
 export default class Minifier {
-  #nameMap = new Map();
-  #alphabet = Array.from('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    #nameMap = new Map();
+    #alphabet = Array.from('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
-  #generateNameIfNotExisting(name) {
-    if (this.#nameMap.has(name)) {
-      return this.#nameMap.get(name);
+    #generateNameIfNotExisting(name) {
+        if (this.#nameMap.has(name)) {
+            return this.#nameMap.get(name);
+        }
+        if (!this.#alphabet) {
+            throw new Error('No more names available - limit is 52 tokens');
+        }
+
+        return this.#alphabet.shift();
     }
-    if (!this.#alphabet) {
-      throw new Error('No more names vailable - limit is 52 tokens');
+    #updateNameMap(oldName, newName, { loc: { start } }) {
+        if (this.#nameMap.has(oldName)) {
+            const nameMap = this.#nameMap.get(oldName);
+            nameMap.positions.push(start)
+            this.#nameMap.set(oldName, nameMap);
+            return;
+        }
+        this.#nameMap.set(oldName, { newName, positions: [start] });
     }
-    const newName = this.#alphabet.shift();
-    this.#nameMap.set(name, alphabet);
-    return newName;
-  }
-  minifyCodeAndReturnMapNames(originalCode) {
-    const originalAST = acorn.parse(originalCode, {ecmaVersion: 2022, locations: true});
-    const minifiedCode = escodegen.generate(originalAST, {format: {compact: true}});
-    console.log('createUsers', this.#generateNameIfNotExisting('createUsers'));
-    console.log('createUsers', this.#generateNameIfNotExisting('createUsers'));
-    console.log('select', this.#generateNameIfNotExisting('createUsers'));
-  }
+
+    #handleDeclaration(declaration) {
+        const oldName = declaration.name;
+        const newName = this.#generateNameIfNotExisting(oldName);
+        this.#updateNameMap(oldName, newName, declaration);
+        declaration.name = newName;
+    }
+    #traverse(node) {
+        const astHelper = new ASTHelper();
+        astHelper
+            .setVariableDeclarationHook(node => {
+                for (const declaration of node.declarations) {
+                    this.#handleDeclaration(declaration.id);
+                }
+            })
+            .setFunctionDeclarationHook(node => {
+                this.#handleDeclaration(node.id);
+                for (const param of node.params) {
+                    this.#handleDeclaration(param);
+                }
+            })
+            .setIdentifierHook(node => {
+                const oldName = node.name;
+                const name = this.#nameMap.get(oldName)?.newName;
+                if (!name) return;
+
+                this.#updateNameMap(oldName, name, node);
+                node.name = name;
+            })
+            .traverse(node);
+    }
+
+    minifyCodeAndReturnMapNames(originalCode) {
+        const originalAST = acorn.parse(originalCode, { ecmaVersion: 2022, locations: true });
+        this.#traverse(originalAST);
+        const minifiedCode = escodegen.generate(originalAST, { format: { compact: true } });
+        return {
+            minifiedCode,
+            nameMap: this.#nameMap
+        }
+    }
 }
